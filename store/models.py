b0068978b05acc68f0598a20bbcf0901877d4b6f
Mihir Verma <mihirverma000@gmail.com>
2026-06-02 22:03:46 +0530

    Add store models for products, customers, orders, and cart

---
+++ store/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+
+# Create your models here.
+
+class Collection(models.Model):
+    title=models.CharField(max_length=255)
+    
+   
+class Product(models.Model):
+    title=models.CharField(max_length=255)
+    description=models.TextField()
+    price = models.DecimalField(max_length=6,decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    Collection=models.ForeignKey(Collection,on_delete=models.PROTECT)
+
+class Customer(models.Model):
+
+    MEMBERSHIP_BRONZE='B'
+    MEMBERSHIP_SILVER='S'
+    MEMBERSHIP_GOLD='G'
+
+    membership_choices=[
+        (MEMBERSHIP_BRONZE,'BRONZE'),
+        (MEMBERSHIP_SILVER,'SILVER'),
+        (MEMBERSHIP_GOLD,'GOLD')
+    ]
+    first_name=models.CharField(max_length=15)
+    last_name=models.CharField(max_length=15)
+    email=models.EmailField(unique=True)
+    phone=models.CharField(max_length=10)
+    birth_date=models.DateField(null=True)
+    membership = models.CharField(max_length=1,choices=membership_choices,default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING='P'
+    PAYMENT_STATUS_COMPLETE='C'
+    PAYMENT_STATUS_FAILED='F'
+
+    PAYMENT_STATUS_CHOICES=[
+        (PAYMENT_STATUS_COMPLETE,'Pending'),
+        (PAYMENT_STATUS_COMPLETE,'Complete'),
+        (PAYMENT_STATUS_FAILED,'Failed')
+    ]
+    order_place_at=models.DateTimeField(auto_now_add=True)
+    payment_status=models.CharField(max_length=1,choices=PAYMENT_STATUS_CHOICES,default=PAYMENT_STATUS_PENDING)
+    customer=models.ForeignKey(Customer,on_delete=models.PROTECT)
+
+class Address(models.Model):
+    street=models.CharField(max_length=255)
+    city=models.ForeignKey(
+        Customer,on_delete=models.CASCADE
+    )
+
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(
+        Order,on_delete=models.PROTECT
+    )
+
+    product=models.ForeignKey(
+        Product,on_delete=models.CASCADE
+    )
+
+    quantity=models.PositiveIntegerField()
+    unit_price=models.DecimalField(max_digits=6,decimal_places=2)
+
+class Cart(models.Model):
+    cart_created_at=models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveSmallIntegerField()
+
+
+
